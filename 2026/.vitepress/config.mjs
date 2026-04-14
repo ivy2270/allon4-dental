d757dd0ceaf49352f0b2c9d6709a2d9b8747f50d
ivy2270 <042703896@yahoo.com.tw>
2026-04-15 00:37:39 +0800

    Update config.mjs

--- 2026/.vitepress/config.mjs
+++ 2026/.vitepress/config.mjs
@@ -1,7 +1,8 @@
 export default {
+  base: '/2026/', // 這一行非常重要，對應你的資料夾路徑
   title: "2026 牙醫年會",
   description: "工作手冊",
   themeConfig: {
-    outline: { label: '本頁導覽', level: [2, 3] } // 自動抓取標題
+    outline: { label: '本頁導覽', level: [2, 3] }
   }
 }
